95794187fc209799410eddb76654cc439e317d3d
josemmartinezc <josemartinez@gmail.com>
2024-02-06 12:44:05 -0400

    aplicando rabbitmq

--- app.js
+++ app.js
@@ -9,3 +9,34 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+const rabbit = require('amqplib');
+const QUEUE_NAME = 'square';
+const EXCHANGE_TYPE = 'direct';
+const EXCHANGE_NAME = 'main';
+const KEY = 'myKey';
+const numbers = ['1', '2', '3', '4', '5']
+
+connection = rabbit.connect('amqp://localhost');
+connection = rabbit.connect('amqp://localhost');
+connection.then(async (conn)=>{
+   const channel = await conn.createChannel();
+   await channel.assertExchange(EXCHANGE_NAME, EXCHANGE_TYPE);
+   await channel.assertQueue(QUEUE_NAME);
+   channel.bindQueue(QUEUE_NAME, EXCHANGE_NAME, KEY);
+   numbers.forEach((number)=>{
+       channel.sendToQueue(QUEUE_NAME, Buffer.from(number))
+   })
+})
+
+
+connection.then(async (conn)=>{
+    const channel = await conn.createChannel();
+    channel.consume(QUEUE_NAME, (m)=>{
+        const number = parseInt(m.content.toString())
+        const square = number * number
+        console.log(square)
+        channel.ack(m)
+    })
+ })
