3f6be5d174ecfb87472cc0bde5240cde155aebcc
josemmartinezc <josemartinez@gmail.com>
2024-02-07 11:52:32 -0400

    avanzando con la creacion de las colas

--- controllers/queqeController.js
+++ controllers/queqeController.js
@@ -4,25 +4,20 @@ var amqp = require('amqplib/callback_api');
 
 class QueqeController {
 
-/**
- * Constructor
- */
-    constructor() { }
-    
     /**
-     * crea la cola si no existe y agrega un elemnto a la cola 
+     * Constructor
+     */
+    constructor() { }
+
+
+    /**
+     * crea una cola
      * @param {*} req 
      * @param {*} res 
-     * @param {*} next 
      */
-
-    async send(req, res, next) {
-
+    async create(req, res){
         var { url } = req.body;
         var { queueName } = req.body;
-        var { message } = req.body;
-        var { durable } = req.body;
-
 
         amqp.connect('amqp://' + url, function (error0, connection) {
             if (error0) {
@@ -32,20 +27,62 @@ class QueqeController {
             }
             connection.createChannel(function (error1, channel) {
                 if (error1) {
-                   // throw error1;
-                   return res.status(404).send('queqe not found')
+                    // throw error1;
+                    return res.status(404).send('queqe not found')
                 }
 
                 channel.assertQueue(queueName, {
-                    durable: durable
+                    durable: true
                 });
-                channel.sendToQueue(queueName, Buffer.from(message));
-                res.status(200).json('queqe created')
-               // console.log(" [x] Sent %s", message);
+                   // channel.sendToQueue(queueName, Buffer.from(message));
+                    res.status(200).json('queqe create')
+                      // console.log(" [x] Sent %s", message);
             });
             setTimeout(function () {
                 connection.close();
-               // process.exit(0);
+                // process.exit(0);
+            }, 500);
+        });
+
+
+
+
+    }
+
+    /**
+     * crea la cola si no existe y agrega un elemnto a la cola 
+     * @param {*} req 
+     * @param {*} res 
+     */
+
+    async send(req, res) {
+
+        var { url } = req.body;
+        var { queueName } = req.body;
+        var { message } = req.body;
+
+        amqp.connect('amqp://' + url, function (error0, connection) {
+            if (error0) {
+                console.error(error0);
+                return res.status(404).send('queqe not found')
+                //throw error0;
+            }
+            connection.createChannel(function (error1, channel) {
+                if (error1) {
+                    // throw error1;
+                    return res.status(404).send('queqe not found')
+                }
+
+                channel.assertQueue(queueName, {
+                    durable: true
+                });
+                    channel.sendToQueue(queueName, Buffer.from(message));
+                    res.status(200).json('message insert')
+                      // console.log(" [x] Sent %s", message);
+            });
+            setTimeout(function () {
+                connection.close();
+                // process.exit(0);
             }, 500);
         });
 
@@ -55,9 +92,8 @@ class QueqeController {
      * obtiene un elemento de la cola
      * @param {*} req 
      * @param {*} res 
-     * @param {*} next 
      */
-    async get(req, res, next) {
+    async get(req, res) {
 
     }
 
@@ -65,9 +101,8 @@ class QueqeController {
      * Elimina una cola y todos sus elementos
      * @param {*} req 
      * @param {*} res 
-     * @param {*} next 
      */
-    async deleteQueqe(req, res, next) {
+    async deleteQueqe(req, res) {
 
     }
 
--- routes/queqe.routers.js
+++ routes/queqe.routers.js
@@ -3,4 +3,5 @@ const router = express.Router();
 var QueqeController  = require('../controllers/queqeController');
 var queqe = new QueqeController();
 router.post('/send', queqe.send);
+router.post('/create', queqe.create);
 module.exports = router;
